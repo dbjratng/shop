a9ddd8048db32fa45599263ffe09600a334b3ff1
keizer <145626895+dbjratng@users.noreply.github.com>
2024-04-19 15:48:54 +0200

    Add files via upload

---
+++ couleur.vue
@@ -0,0 +1,13 @@
+<template>
+  <CustomComponent customBackgroundColor="red" customTextColor="white" text="Hello!" logoUrl="C:\app\image" />
+</template>
+
+<script lang="ts">
+import CustomComponent from './CustomComponent.vue';
+
+export default {
+  components: {
+    CustomComponent,
+  },
+};
+</script>
